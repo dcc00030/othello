3434bef3d6b01f6de821ab7a3e2975892d926210
dcasquel <dcasquel@Daniel>
2016-05-03 16:54:30 +0200

    todo acabado

--- src/othello/Utils/Heuristica.java
+++ src/othello/Utils/Heuristica.java
@@ -5,18 +5,37 @@
 
 package othello.Utils;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author user
  */
 public class Heuristica {
-
-
-    public static int h1(Tablero tablero)
+    
+    public static int h2(Tablero tablero,int playerColor)
     {
-        return 0;
+        //da un valor a cada una de las posiciones del tablero
+        //dandole mayor valoracion a las esquinas del tablero
+        int[][] matriz = {{6,-4,5,3,3,5,-4,6},{5,-3,4,2,2,4,-3,5},{4,-2,3,1,1,3,-2,4},{3,-1,2,0,0,2,-1,3},{3,-1,2,0,0,2,-1,3},{4,-2,3,1,1,3,-2,4},{5,-3,4,2,2,4,-3,5},{6,-4,5,3,3,5,-4,6}};
+        //usamos el mismo metodo anterior para calcular los puntos que tenemos
+        //contando la posicion de las fichas
+        int puntuacion = Puntos(playerColor,tablero) -Puntos(-playerColor,tablero);
+        ArrayList<Casilla> posiblesMovimientos = tablero.generarMovimiento(playerColor);
+        puntuacion = posiblesMovimientos.size();
+        //y aqui usamos la puntuacion de los posibles movimientos junto con el valor
+        //introducido en la matriz creada por nosotros
+        Casilla [][]matrizAux = tablero.getMatrizTablero();
+        for(int  i = 0; i < 8; i++){
+            for(int j = 0; j < 8; j++){
+                if(matrizAux[i][j].obtenerColorFicha()== playerColor){
+                    puntuacion= puntuacion + matriz[i][j];
+                }
+            }
+        }
+        return puntuacion;
     }
-
+/*
 
     //Una heuristica posible a usar
     public static int h2(Tablero tablero,int playerColor)
@@ -37,7 +56,7 @@ public class Heuristica {
         // if game isn't over, return relative advatage
         return score;
     }
-
+*/
     public static int Puntos(int playerColor, Tablero tablero)
     {
         int points = 0;
@@ -50,10 +69,7 @@ public class Heuristica {
         return points;
     }
 
-    public static int h1(Tablero tablero, int playerColor) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
+    
 
 
 }
--- src/othello/algoritmo/AlgoritmoMiniMax.java
+++ src/othello/algoritmo/AlgoritmoMiniMax.java
@@ -1,74 +1,116 @@
 /*
  */
-
 package othello.algoritmo;
 
 import othello.Utils.Casilla;
 import othello.Utils.Heuristica;
 import othello.Utils.Tablero;
 import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+import static othello.Utils.Casilla.FICHA_BLANCA;
 
 /**
  *
  * @author gusamasan
  */
-public class AlgoritmoMiniMax extends Algoritmo{
+public class AlgoritmoMiniMax extends Algoritmo {
 // ----------------------------------------------------------------------
 
 // ----------------------------------------------------------------------
-
-    /** Constructores **************************************************/
+    /**
+     * Constructores *************************************************
+     */
     private int playerColor;
-    public AlgoritmoMiniMax(){
+
+    public AlgoritmoMiniMax() {
 
     }
-    /*******************************************************************/
-    
 
+    /**
+     * ****************************************************************
+     */
     @Override
-    public Tablero obtenerNuevaConfiguracionTablero( Tablero tablero, short turno ){
+    public Tablero obtenerNuevaConfiguracionTablero(Tablero tablero, short turno) {
 
-        System.out.println( "analizando siguiente jugada con MINIMAX" );
-        this.playerColor=turno;
-        Tablero tableroJugada=tablero.copiarTablero();
-         try{
-             miniMax(tableroJugada, this.getProfundidad(), playerColor);
-            Thread.sleep( 1000 );
-        }
-        catch( Exception e ){
+        System.out.println("analizando siguiente jugada con MINIMAX");
+        this.playerColor = turno;
+        Tablero tableroJugada = tablero.copiarTablero();
+        try {
+            miniMax(tableroJugada, this.getProfundidad(), playerColor);
+            Thread.sleep(1000);
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
-        return( tableroJugada );
+        return (tableroJugada);
     }
 
-     /**
+    /**
      *
      * Éste es el método que tenemos que implementar.
-     * 
+     *
      * Algoritmo AlfaBeta para determinar cuál es el siguiente mejor movimiento
-     * 
-     * @param tablero
-     * Configuración actual del tablero
-     * @param prof
-     * Profundidad de búsqueda
-     * @param jugadorActual
-     * Nos indica a qué jugador (FICHA_BLANCA ó FICHA_NEGRA) le toca
+     *
+     * @param tablero Configuración actual del tablero
+     * @param prof Profundidad de búsqueda
+     * @param jugadorActual Nos indica a qué jugador (FICHA_BLANCA ó
+     * FICHA_NEGRA) le toca
      * @return
      */
-    public int miniMax(Tablero tablero, int prof, int jugadorActual)
-    {        
-        int fila,col;
-        Casilla cas;
+    public int miniMax(Tablero tablero, int prof, int jugadorActual) {
+
+        //Nos pasan un tablero y el turno.
+        //Calculamos la mejor jugada para nuestro turno
+        ArrayList<Casilla> casillas = tablero.generarMovimiento(jugadorActual); //Posibles casillas donde poner una ficha        
+        ArrayList<Integer> valor = new ArrayList<Integer>(); //Valor del tablero resultado de poner la ficha de la posicion i
+
+        Tablero copia = tablero.copiarTablero();
+
+        //Si no es nodo hoja o maxima profundidad, calculamos siguiente nivel
+        if (prof > 0) {
+            for (int i = 0; i < casillas.size(); i++) {
+
+                if (jugadorActual == FICHA_BLANCA) {
+                    casillas.get(i).asignarFichaBlanca();
+                } else {
+                    casillas.get(i).asignarFichaNegra();
+                }
+
+                copia.ponerFicha(casillas.get(i));
+
+                valor.add(miniMax(copia, prof-1, (-1)*jugadorActual));
+
+                copia = tablero.copiarTablero();
+                
+            }
+        }
+
+        //Si hemos vuelto a la llamada inicial
+        if (prof == 5) {
+            
+            Casilla mejorCasilla = casillas.get(0).copiarCasilla();
+            int max = valor.get(0);
+           
+            if (jugadorActual == FICHA_BLANCA) {
+                for (int i = 0; i < valor.size(); i++) {
+                    if (valor.get(i) > max) {
+                        max = valor.get(i);
+                        mejorCasilla = casillas.get(i).copiarCasilla();
+                    }
+                }
+            } else {
+                for (int i = 0; i < valor.size(); i++) {
+                    if (valor.get(i) < max) {
+                        max = valor.get(i);
+                        mejorCasilla = casillas.get(i).copiarCasilla();
+                    }
+                }
+            }
+
+            tablero.ponerFicha(mejorCasilla);
+        }
         
-        ArrayList<Casilla> movimientos = new ArrayList<>();
-        movimientos=tablero.generarMovimiento(jugadorActual);
-        //
-        int movimiento = 1;
-		// ...
-        
-        // Devolver el valor para el mejor movimiento
-        return movimiento;
-        
+        return Heuristica.h2(tablero,playerColor);
+
     }
 }
